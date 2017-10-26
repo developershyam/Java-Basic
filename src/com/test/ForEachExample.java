01d7e910e87eaf7a3e6c5f7910463e390b4d6cbb
Shyam Pareek <shyam@zudento.com>
2017-10-26 19:35:22 +0530

    basic program added

---
+++ src/com/test/ForEachExample.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class ForEachExample {
+	public static void main(String[] args) {
+		int arr[] = { 12, 23, 44, 56, 78 };
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+}
+
+
+//output
+//12
+//23
+//44
+//56
+//78
